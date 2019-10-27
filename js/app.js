d288c97707a30c16e9686bcb908cfb644ba0065f
monazo22 <46850877+monazo22@users.noreply.github.com>
2019-10-27 19:26:08 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+window.onscroll = function(e){
+    let scroll = window.scrollY;
+    const headerScroll = document.querySelector('.header-scroll');
+    if(scroll > 300){
+        headerScroll.classList.add('activo');
+    }else{
+        headerScroll.classList.remove('activo');
+    }
+}
